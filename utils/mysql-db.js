b783787d9ad6b6e6be7db8cd286e7f1ae6a4fea6
JanvElswijk <jajp.vanelswijk@student.avans.nl>
2023-05-21 16:19:38 +0200

    WHAT THE FUCKKKKKKKKKKKKKKKKKK

--- utils/mysql-db.js
+++ utils/mysql-db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD || '',
     multipleStatements: true,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 30,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
     idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
     queueLimit: 0
@@ -26,8 +26,8 @@ const query = (sql, params, callback) => {
             } else {
                 callback(null, rows);
             }
-            connection.release();
         });
+        connection.release();
     });
 };
 
