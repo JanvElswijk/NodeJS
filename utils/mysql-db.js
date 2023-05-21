4851eb6e8da5bc8abf825d038a5a11cc40c07077
JanvElswijk <jajp.vanelswijk@student.avans.nl>
2023-05-21 18:30:39 +0200

    kanker

--- utils/mysql-db.js
+++ utils/mysql-db.js
@@ -8,8 +8,8 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD || '',
     multipleStatements: true,
     waitForConnections: true,
-    connectionLimit: 30,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    connectionLimit: 100,
+    maxIdle: 100, // max idle connections, the default value is the same as `connectionLimit`
     idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
     queueLimit: 0
 });
